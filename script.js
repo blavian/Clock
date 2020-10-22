94d3bffafa0e6b0f110a680493fa0029b3c6d6cc
Benjamin Lavian <benylavian@gmail.com>
2020-10-21 22:58:16 -0400

    created setClock function

---
+++ script.js
@@ -0,0 +1,8 @@
+setInterval(setClock, 1000)
+
+function setClock(){
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+}
